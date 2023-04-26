73efa50b7cfdef9909b717b858676b50f9e86b6f
Sergio David Latorre Guevara <sergio.latorre-guevara@siemens-energy.com>
2023-04-26 13:32:50 -0500

    Add classes: Employee, SalaryEmployee, CommissionEmployee

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("POO CONCEPTS");
+Console.WriteLine("============");
+
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1010,
+    FirstName = "Sergio",
+    LastName = "Latorre",
+    BirthDate = new Date(1987, 6, 12),
+    HiringDate = new Date(2010, 5, 4),
+    IsActive = true,
+    Salary = 2290000.45M
+};
+
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 1020,
+    FirstName = "David",
+    LastName = "Latorre",
+    BirthDate = new Date(1988, 5, 7),
+    HiringDate = new Date(2020, 5, 4),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+};
+
+Console.WriteLine(employee2);
